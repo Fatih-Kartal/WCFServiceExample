e0d95a02083d0c5fe978ef33f05fdbbac682ef8e
Fatih Kartal <fatih.kartal@mobven.com>
2023-03-30 18:28:17 +0300

    Added WCFService

---
+++ WCFService/WebService.asmx.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Services;
+
+namespace WCFService
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld(string name)
+        {
+            return string.Format("Hello {0}", name);
+        }
+    }
+}
